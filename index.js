0343adc12368cd5043af16f82bff14b429f11ec4
MoeyR <ranm@sheridancollege.ca>
2024-03-09 16:58:19 -0500

    Used CORS to allow frontend app to acess resource; Placed PORT and CORS_ORIGIN in .env

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express');
-const cors = require('cors');
 const videoRoutes = require('./routes/videos');
-const app = express();
+const cors = require('cors');
+require('dotenv').config();
 
+
+const app = express();
+const PORT = process.env.PORT || 8081;
+const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:3001";
 app.use(express.static('public'));
 
-app.use(cors());
+app.use(cors({origin: CORS_ORIGIN}));
 app.use(express.json());
 app.use('/videos', videoRoutes);
 
 app.listen(8080, function(){
-    console.log("Server is now listening at http://localhost:8080");
+    console.log(`Server is now listening on port ${PORT}`);
 });
 
 
